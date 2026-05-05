4812d1cb998d996b51d6dead83267ee47e67c998
Salvador <salvador2121212121@gmail.com>
2026-05-05 17:26:44 -0600

    fix: no-cache en todas las rutas API

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Cache-Control',             value: 'no-store, no-cache, must-revalidate, max-age=0' },
+          { key: 'Vercel-CDN-Cache-Control',  value: 'no-store' },
+          { key: 'CDN-Cache-Control',         value: 'no-store' },
+          { key: 'Surrogate-Control',         value: 'no-store' },
+          { key: 'Pragma',                    value: 'no-cache' },
+        ],
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
